b24eddb5d67ede4a5f57d7171e1046091d359596
k_h <K_Haruka03@outlook.jp>
2024-03-20 00:31:29 +0900

    フロントエンドのみデプロイ用に変更した

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import Login from './pages/Login'
-import List from './pages/List'
-import Delete from './pages/Delete'
+// import Login from './pages/Login'
+// import List from './pages/List'
+// import Delete from './pages/Delete'
 import Design from './pages/Design'
 import NoMatch from './pages/NoMatch'
-import Sample from './pages/Sample'
+// import Sample from './pages/Sample'
 
 function App (): JSX.Element {
   return (
     <div className='App'>
       <ChakraProvider>
         <Routes>
-          <Route path='/' element={<Login/>} />
-          <Route path='/list' element={<List/>} />
-          <Route path='/delete' element={<Delete/>} />
-          <Route path='/design' element={<Design/>} />
+          <Route path='/' element={<Design/>} />
+          {/* <Route path='/list' element={<List/>} /> */}
+          {/* <Route path='/delete' element={<Delete/>} /> */}
+          {/* <Route path='/design' element={<Design/>} /> */}
           <Route path='*' element={<NoMatch/>} />
-          <Route path='/sample' element={<Sample/>} />
+          {/* <Route path='/sample' element={<Sample/>} /> */}
         </Routes>
       </ChakraProvider>
     </div>
